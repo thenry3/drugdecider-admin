49af047b1131ba209b351695928b15e92c93c3e6
Dhruv Sharma <dhruvsharma0503@gmail.com>
2020-03-03 16:58:46 -0800

    small fix again lol

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.static(__dirname + '/views'));
 /* MONGOOSE SETUP */
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/drugdecider');
+mongoose.connect('mongodb://localhost:27017/drugdecider');
 
 const Schema = mongoose.Schema;
 const UserDetail = new Schema({
