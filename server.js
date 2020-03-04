b5ae54b56d38ad24c3637a88d1a409a2026e08f0
Dhruv Sharma <dhruvsharma0503@gmail.com>
2020-03-03 18:32:03 -0800

    csp fix

--- server.js
+++ server.js
@@ -10,6 +10,68 @@ const session = require('express-session');
 const methodOverride = require('method-override');
 const writeFileAtomic = require('write-file-atomic');
 const path = require('path');
+const csp = require('helmet-csp');
+
+app.use(
+  csp({
+    // Specify directives as normal.
+    directives: {
+      defaultSrc: [
+        "'self'",
+        'https://api.drugdecider.com',
+        'https://api.drugdecider.com/api/v1/druginfo',
+      ],
+      scriptSrc: [
+        "'self'",
+        "'unsafe-inline'",
+        'https://api.drugdecider.com',
+        'https://api.drugdecider.com/api/v1/druginfo',
+      ],
+      styleSrc: [
+        "'self'",
+        "'unsafe-inline'",
+        'https://api.drugdecider.com',
+        'https://api.drugdecider.com/api/v1/druginfo',
+      ],
+      fontSrc: [
+        "'self'",
+        'https://api.drugdecider.com',
+        'https://api.drugdecider.com/api/v1/druginfo',
+      ],
+      imgSrc: [
+        "'self'",
+        'https://api.drugdecider.com',
+        'https://api.drugdecider.com/api/v1/druginfo',
+      ],
+      sandbox: ['allow-forms', 'allow-scripts'],
+      reportUri: '/report-violation',
+      objectSrc: ["'none'"],
+      upgradeInsecureRequests: true,
+      workerSrc: false, // This is not set.
+    },
+
+    // This module will detect common mistakes in your directives and throw errors
+    // if it finds any. To disable this, enable "loose mode".
+    loose: false,
+
+    // Set to true if you only want browsers to report errors, not block them.
+    // You may also set this to a function(req, res) in order to decide dynamically
+    // whether to use reportOnly mode, e.g., to allow for a dynamic kill switch.
+    reportOnly: false,
+
+    // Set to true if you want to blindly set all headers: Content-Security-Policy,
+    // X-WebKit-CSP, and X-Content-Security-Policy.
+    setAllHeaders: false,
+
+    // Set to true if you want to disable CSP on Android where it can be buggy.
+    disableAndroid: false,
+
+    // Set to false if you want to completely disable any user-agent sniffing.
+    // This may make the headers less compatible but it will be much faster.
+    // This defaults to `true`.
+    browserSniff: true,
+  })
+);
 
 // Add some body parser middleware
 app.use(express.json());
