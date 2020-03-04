25f128ebd77f5d8887473cda1d7a00062866b4e5
Dhruv Sharma <dhruvsharma0503@gmail.com>
2020-03-03 20:04:36 -0800

    somehow this fixes it

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(
         "'self'",
         'https://api.drugdecider.com',
         'https://api.drugdecider.com/api/v1/druginfo',
-        'https://admin.drugdecider.com/updatedata',
+        'https://admin.drugdecider.com/updatedata/',
       ],
       styleSrc: [
         "'self'",
